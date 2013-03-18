ddf450ee8f4db068765ffe32e3ada8d5ac28a050
Adar Porat <hadar@mybuilding.org>
2013-03-18 17:11:13 -0400

    reverting changes to MessageClientTest

--- tests/ZendService/Apple/Apns/MessageClientTest.php
+++ tests/ZendService/Apple/Apns/MessageClientTest.php
@@ -2,16 +2,19 @@
 /**
  * Zend Framework (http://framework.zend.com/)
  *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Service
+ * @link       http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright  Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license    http://framework.zend.com/license/new-bsd New BSD License
+ * @category   ZendService
+ * @package    ZendService_Apple
+ * @subpackage UnitTests
  */
 
 namespace ZendServiceTest\Apple\Apns;
 
 use ZendServiceTest\Apple\Apns\TestAsset\MessageClient;
 use ZendService\Apple\Apns\Message;
+use ZendService\Apple\Apns\Message\Alert;
 use ZendService\Apple\Apns\Response\Message as MessageResponse;
 
 /**
@@ -88,79 +91,88 @@ class MessageClientTest extends \PHPUnit_Framework_TestCase
     {
         $this->setupValidBase();
         $response = $this->apns->send($this->message);
-        $this->assertTrue($response);
+        $this->assertEquals(MessageResponse::RESULT_OK, $response->getCode());
+        $this->assertNull($response->getId());
     }
-
 
     public function testSendResponseOnProcessingError()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 1, 1, 12345));
-       	$response = $this->apns->send($this->message);
+        $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_PROCESSING_ERROR, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 
     public function testSendResponseOnMissingToken()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 2, 2, 12345));
         $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_MISSING_TOKEN, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 
     public function testSendResponseOnMissingTopic()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 3, 3, 12345));
         $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_MISSING_TOPIC, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 
     public function testSendResponseOnMissingPayload()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 4, 4, 12345));
         $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_MISSING_PAYLOAD, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 
     public function testSendResponseOnInvalidTokenSize()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 5, 5, 12345));
         $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_INVALID_TOKEN_SIZE, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 
     public function testSendResponseOnInvalidTopicSize()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 6, 6, 12345));
         $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_INVALID_TOPIC_SIZE, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 
     public function testSendResponseOnInvalidPayloadSize()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 7, 7, 12345));
         $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_INVALID_PAYLOAD_SIZE, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 
     public function testSendResponseOnInvalidToken()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 8, 8, 12345));
         $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_INVALID_TOKEN, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 
     public function testSendResponseOnUnknownError()
     {
-        $this->setExpectedException('RuntimeException');
-    	$this->setupValidBase();
+        $this->setupValidBase();
         $this->apns->setReadResponse(pack('CCN*', 255, 255, 12345));
         $response = $this->apns->send($this->message);
+        $this->assertEquals(MessageResponse::RESULT_UNKNOWN_ERROR, $response->getCode());
+        $this->assertEquals(12345, $response->getId());
     }
 }
